82cbffa6418a93da2b79ae71939687ce1229dc73
2hye1st <2hye1st@naver.com>
2019-10-29 15:54:47 +0900

    ADD: all files

---
+++ app/src/main/java/com/example/studywithme_hey1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.studywithme_hey1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
